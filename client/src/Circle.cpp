256ca5f1ca16a49ab0331495d1617dda525358e2
Lassi Lääti <lassilaati@gmail.com>
2016-02-14 17:21:28 +0200

    add boundaries to local position of player

--- client/src/Circle.cpp
+++ client/src/Circle.cpp
@@ -11,6 +11,8 @@ void Circle::update() {
 	// if gameobject is not static it should try to move somewhere
 	if(!isStatic) {
 		m_speed = (1-(log(m_sr/100.0f)/log(1024)))*5.0f;
+		// if respawning or something else happened that we are really far away
+		// just jump there
 		if(abs(m_sx-m_x)>100 || abs(m_sy-m_y)>100) {
 			m_x = m_sx;
 			m_y = m_sy;
@@ -22,8 +24,11 @@ void Circle::update() {
 		// update drawable sphere
 		m_x = (m_x + ((m_sx-m_x)/10));
 		m_y = (m_y + ((m_sy-m_y)/10));
-		// if respawning or something else happened that we are really far away
-		// just jump there
+		// dont let m_x and m_y get over uint16_t boundaries
+		if(m_x < 0)m_x = 0;
+		if(m_y < 0)m_y = 0;
+		if(m_x > 32766) m_x = 32766;
+		if(m_y > 32766) m_y = 32766;
 	}
 	m_destRect = {
 		int(m_x), int(m_y),        		// make center of circle true center of drawed texture
